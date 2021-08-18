bdcdc725bd5613f1d0f4689d8c07b0bbd9ed1172
Luciano Santos <lucianoliv2@hotmail.com>
2021-08-17 21:50:18 -0300

    Add files via upload

---
+++ Php/codigo.php
@@ -0,0 +1,19 @@
+<?php
+
+$url = "https://swapi.dev/api/starships";
+
+$results = json_decode(file_get_contents($url));
+
+
+//var_dump($results);
+foreach ($results->results as $nome)
+{
+    echo "personagem : ". $nome->name."<br>";
+}
+
+
+
+//$result  = json_decode($content);
+
+
+
